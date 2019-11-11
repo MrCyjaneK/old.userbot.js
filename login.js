4ab73857ad0c2121605ef130690eb5a29a40fbce
mrcyjanek <mrcyjanek@gmail.com>
2019-11-11 23:17:45 +0100

    added login feature

---
+++ login.js
@@ -0,0 +1,21 @@
+const prompts = require('prompts');
+const fs      = require("fs");
+const login   = require("facebook-chat-api");
+(async () => {
+  const response = await prompts([{
+    type: 'text',
+    name: 'email',
+    message: 'Facebook email'
+  },
+  {
+  	type: 'text',
+  	name: 'password',
+  	message: 'Facebook password'
+  }]);
+  console.log(response);
+  login({email: response.email, password: response.password}, (err, api) => {
+    if(err) return console.error(err);
+
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+});
+})();
