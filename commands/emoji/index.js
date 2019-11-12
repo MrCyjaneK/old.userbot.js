c402ca83ab74ed776b31dff510538dd711647965
mrcyjanek <mrcyjanek@gmail.com>
2019-11-12 06:52:49 +0100

    bugfix

--- commands/emoji/index.js
+++ commands/emoji/index.js
@@ -1,7 +1,6 @@
 module.exports = ((api,message,user) => {
     randemoji = require('emojis-list')[Math.floor(Math.random() * require('emojis-list').length)];
     api.changeThreadEmoji(randemoji, message.threadID, (err) => {
-        if(err) return 
-        api.sendMessage(err.toString(),message.threadID);
+        if(err) return api.sendMessage(err.toString(),message.threadID);
     });
 })
