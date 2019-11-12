f3a7fea24cb0fbf2010470b182ea6e869fac9164
mrcyjanek <mrcyjanek@gmail.com>
2019-11-12 01:30:32 +0100

    added emoji

--- commands/ping/index.js
+++ commands/ping/index.js
@@ -1,10 +1,10 @@
 module.exports = (async (api,message,user) => {
     var t0 = new Date().getTime();
     await ((api,message) => {
-        api.sendMessage("Pong!", message.threadID);
+        api.sendMessage("🏓Pong!", message.threadID);
     })(api,message)
     var t1 = new Date().getTime();
     setTimeout(() => {
-        api.sendMessage("Reply took: "+Number(t1-t0).toFixed(2)+"ms", message.threadID);
+        api.sendMessage("🏓Reply took: "+Number(t1-t0).toFixed(2)+"ms", message.threadID);
     },2500);
 })
