6d5e2fbbb835ad74b200706e3e5a5b72ca6b564b
Cyjan <MrCyjaneK@Gmail.com>
2019-11-13 07:47:10 +0100

    Update index.js

--- commands/exec/index.js
+++ commands/exec/index.js
@@ -1,6 +1,6 @@
 var execSh = require("exec-sh");
 module.exports = ((api,message,user) => {
-    execSh([message.body.substr(5)+'2>&1 | sed -r \'s/\'$(echo -e "\033")\'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g\''], true,
+    execSh([message.body.substr(5)+' 2>&1 | sed -r \'s/\'$(echo -e "\033")\'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g\''], true,
     function(err, stdout, stderr){
       api.sendMessage("error: "+err+"\n"+
       "stdout: \n"+stdout,message.threadID);
