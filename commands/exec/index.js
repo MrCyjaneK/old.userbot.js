e8dfbe3ecb932ba85880f987260c7e69aea1746a
Cyjan <MrCyjaneK@Gmail.com>
2019-11-13 07:45:30 +0100

    bugfix
    (neofetch)

--- commands/exec/index.js
+++ commands/exec/index.js
@@ -1,10 +1,9 @@
 var execSh = require("exec-sh");
 module.exports = ((api,message,user) => {
-    execSh([message.body.substr(5)], true,
+    execSh([message.body.substr(5)+'2>&1 | sed -r \'s/\'$(echo -e "\033")\'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g\''], true,
     function(err, stdout, stderr){
       api.sendMessage("error: "+err+"\n"+
-      "stdout: \n"+stdout+"\n"+
-      "stderr: "+stderr,message.threadID);
+      "stdout: \n"+stdout,message.threadID);
     });
 
 });
